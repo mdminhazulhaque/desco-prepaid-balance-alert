a75e45c6e806a92f6902f8f626274812effa8894
Md. Minhazul Haque <1847242+mdminhazulhaque@users.noreply.github.com>
2024-06-25 08:01:27 +0600

    Update desco.py

--- desco.py
+++ desco.py
@@ -34,5 +34,7 @@ msg.attach(MIMEText(body, 'plain'))
 
 # fire
 server = smtplib.SMTP(SMTP_HOST, SMTP_PORT)
+server.ehlo()
+server.starttls()
 server.login(SMTP_USER, SMTP_PASSWORD)
 server.sendmail(SMTP_FROM_EMAIL, DESCO_PREPAID_USER_EMAIL, msg.as_string())
